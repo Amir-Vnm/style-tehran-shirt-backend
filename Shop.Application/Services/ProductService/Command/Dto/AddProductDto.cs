40d79200e6627ce23f6e2221a8df5427c4b4bfb5
Amir-Vnm <149929776+Amir-Vnm@users.noreply.github.com>
2025-10-20 14:05:35 +0000

    Update AddProductDto.cs

--- Shop.Application/Services/ProductService/Command/Dto/AddProductDto.cs
+++ Shop.Application/Services/ProductService/Command/Dto/AddProductDto.cs
@@ -1,13 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace Shop.Application.Services.ProductService.Command.Dto
+public class AddProductDto
 {
-    public record AddProductDto
-    {
-        public string Description { get; set; }
-        public IFormFile ImageFile { get; set; }
-        public double Price { get; set; }
-        public int inventory { get; set; }
-        public long CategoryId { get; set; }
-    }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public long CategoryId { get; set; }
+    public double Price { get; set; }
+    public int inventory { get; set; }
+
+    public IFormFile Picture { get; set; } // عکس از فرانت
+    public string PictureUrl { get; set; } // لینک Cloudinary
 }
